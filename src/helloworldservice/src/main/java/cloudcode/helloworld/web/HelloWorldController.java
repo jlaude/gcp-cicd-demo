457353567901bb8df52e5b03c8e152b0c4ba5e32
John Laude <admin@jlaude.altostrat.com>
2022-12-05 15:46:13 +0000

    extenalized application.properties via configmap

--- src/helloworldservice/src/main/java/cloudcode/helloworld/web/HelloWorldController.java
+++ src/helloworldservice/src/main/java/cloudcode/helloworld/web/HelloWorldController.java
@@ -4,9 +4,11 @@ package cloudcode.helloworld.web;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 /**
  * Defines a controller to handle HTTP requests.
@@ -14,13 +16,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public final class HelloWorldController {
 
+    @Value("${PROJECT_ID}")
+    private String project_id;
+
     /**
      * Create an endpoint for the landing page
      * @return the index view template with a simple message
      */
     @GetMapping("/")
     public String helloWorld(Model model) {
-        String message = "It's Running!";
+
+
+        //String message = "It's Running!";
+        String message = project_id;
         model.addAttribute("message", message);
 
         InetAddress ip;
